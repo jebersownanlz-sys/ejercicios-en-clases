9b6fd9481699bd266b61f24d5bc50543dd1bde5d
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 18:55:52 -0500

    Add program to round a number using Math.Round()

---
+++ ejercicio11.cs
@@ -0,0 +1,13 @@
+//Math.Round()redondea un número al número entero más cercano:
+using System;
+
+namespace ejercicio11
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Round(9.99));
+     }
+  }
+}
