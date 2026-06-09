b72eb817130c6ea4d3bd7b879d22cd2fd4cbf844
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 18:54:34 -0500

    Add ejercicio1.cs with implicit type conversion example

---
+++ ejercicio1.cs
@@ -0,0 +1,15 @@
+using System;
+class ejercicio1 {
+  static void Main() {
+      //1. Definicion del dato inicial (menor tamaño)
+      int myInt = 9;
+      
+      //2. FUNDICION IMPLICITA (Automatica)
+      double myDouble = myInt;
+      
+      //3. Verificacion en Consola
+      
+    Console.WriteLine(myInt);
+    Console.WriteLine(myDouble);
+  }
+}
