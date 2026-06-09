67533a07bd8432163615eb8da723de5b6c15f94c
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 18:55:10 -0500

    Create ejercicio10.cs

---
+++ ejercicio10.cs
@@ -0,0 +1,13 @@
+//El método devuelve el valor absoluto (positivo) de x :Math.Abs(x)
+using System;
+
+namespace ejercicio10
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Abs(-4.7));  
+     }
+  }
+}
