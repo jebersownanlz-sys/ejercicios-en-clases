73a883618cf0f3fbb613a10fb4300a3117b2245c
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:26:34 -0500

    Create ejercicio7.cs

---
+++ ejercicio7.cs
@@ -0,0 +1,13 @@
+//Este método se puede utilizar para encontrar el valor más alto de x e y :Math.Max(x,y)
+using System;
+
+namespace ejercicio7
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Max(5, 10));  
+     }
+  }
+}
