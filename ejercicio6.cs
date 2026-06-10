a94fbde9123a615ac6941954f3c4990c2cd35383
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:26:11 -0500

    Add ejercicio6.cs with comparison logic

---
+++ ejercicio6.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class ejercicio4
+{
+    public static void Main(string[] args)
+    {
+        int x = 5;
+        int y = 3;
+
+
+        Console.WriteLine(x>y);
+    }
+}
