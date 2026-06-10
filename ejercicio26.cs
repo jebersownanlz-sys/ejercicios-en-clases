9dd3a4262764bf5f53965f52e5a6fe799704f2b3
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:19:47 -0500

    Add Program class to display backslash character

---
+++ ejercicio26.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ejercicio26
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string txt = "The character \\ is called backslash.";
+      Console.WriteLine(txt);
+    }
+  }
+}
