b9c9fbc09b05a146f2a5bc618ad7d7febc628755
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:14:47 -0500

    Add initial implementation of ejercicio24.cs

---
+++ ejercicio24.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ejercicio24
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string txt = "We are the so-called \"Vikings\" from the north.";
+      Console.WriteLine(txt);
+    }
+  }
+}
