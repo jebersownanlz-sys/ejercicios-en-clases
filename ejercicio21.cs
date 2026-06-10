9875885d3924ed1f688760d75bd11da3478e356c
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:13:53 -0500

    Add Program class to print character from string

---
+++ ejercicio21.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ejercicio21
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string myString = "Hello";
+      Console.WriteLine(myString[3]);
+    }
+  }
+}
