33546b85277073306fe2faa8abc62b2f315188f4
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:24:28 -0500

    Create ejercicio4.1.cs

---
+++ ejercicio4.1.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class ejercicio4
+{
+    public static void Main(string[] args)
+    {
+        int sum1 = 100 + 50;
+        double sum2 = sum1 + 534.71;
+        double sum3 = sum1 + sum2;
+        
+        Console.WriteLine(sum1);
+        Console.WriteLine(sum2);
+        Console.WriteLine(sum3);
+    }
+}
