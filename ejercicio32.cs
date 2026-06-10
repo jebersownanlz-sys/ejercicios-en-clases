513bf323149d4b5d39a13079346427765c576ed8
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:21:41 -0500

    Add ejercicio32.cs with time-based greeting

---
+++ ejercicio32.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class ejercicio32
+{
+    public static void Main(string[] args)
+    {
+        int time = 20; 
+        if (time<18){
+        Console.WriteLine ("Good day");
+        } else {
+        Console.WriteLine ("Good evening");
+        }
+    }
+}
