9d5c74b348cea68b1d0ff0e38f6771ebee0686fd
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:27:17 -0500

    Add program to calculate square root of 64

---
+++ ejercicio9.cs
@@ -0,0 +1,13 @@
+//El método devuelve la raíz cuadrada de x :Math.Sqrt(x)
+using System;
+
+namespace ejercicio9
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Sqrt(64));  
+     }
+  }
+}
