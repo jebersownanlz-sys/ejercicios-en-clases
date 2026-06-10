8717bdaac222086583390ce4bfb842cf3fc08b65
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:19:23 -0500

    Add basic C# program to print a string

---
+++ ejercicio25.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ejercicio25
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string txt = "It\'s alright.";
+      Console.WriteLine(txt);
+    }
+  }
+}
