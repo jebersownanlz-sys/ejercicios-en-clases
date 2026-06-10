029b93ed148053be3a8bdc95c52f2f013abd0d0f
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:10:17 -0500

    Create ejercicio16.cs

---
+++ ejercicio16.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ejercicio16
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string firstName = "John ";
+      string lastName = "Doe";
+      string name = firstName + lastName;
+      Console.WriteLine(name);
+    }
+  }
+}
