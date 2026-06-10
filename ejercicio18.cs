3ac0006483507f1541fd36c766ea0ef0a9e2d1b5
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:10:54 -0500

    Add basic addition program in ejercicio18.cs

---
+++ ejercicio18.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ejercicio18
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 10;
+      int y = 20;
+      int z = x + y;
+      Console.WriteLine(z);
+    }
+  }
+}
