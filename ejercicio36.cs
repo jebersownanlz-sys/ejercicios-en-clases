2713ba11a94da0155d11f16cf5483d136cbcb1ac
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:23:18 -0500

    Add HelloWorld program to print numbers 0-4

---
+++ ejercicio36.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int  i = 0;
+        
+        while (i < 5) {
+            Console.WriteLine(i);
+            i++;//incremento
+        }
+    }
+}
