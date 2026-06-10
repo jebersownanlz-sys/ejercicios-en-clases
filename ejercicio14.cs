f32c219776453f15b591c5f35ef6d9d440167bc7
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:09:26 -0500

    Add Program class to display string length

---
+++ ejercicio14.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ejercicio14
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      Console.WriteLine("The length of the txt string is: " + txt.Length);
+    }
+  }
+}
