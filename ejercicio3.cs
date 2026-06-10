c8b3033c6e3a2c4fa97f3cc8d2045dca0af2a1d4
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:20:53 -0500

    Add ejercicio3.cs to demonstrate basic addition

---
+++ ejercicio3.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class ejercicio3
+{
+    public static void Main(string[] args)
+    {
+        //int x = 100 + 50;
+        double x = 3.5 + 4.75;
+        
+        Console.WriteLine(x);
+    }
+}
