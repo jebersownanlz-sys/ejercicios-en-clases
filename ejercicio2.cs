42c58379e0f53ddfd2baa84f186320f3159cba35
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:12:38 -0500

    Add ejercicio2.cs for type casting demonstration

---
+++ ejercicio2.cs
@@ -0,0 +1,11 @@
+using System;
+class ejercicio2 {
+  static void Main() {
+     double myDouble = 9.78;
+     
+     int myInt = (int) myDouble;
+     
+     Console.WriteLine(myDouble);
+     Console.WriteLine(myInt);
+  }
+}
