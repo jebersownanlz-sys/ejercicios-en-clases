2797ba5758fc2e9fbb6d9e06a00906feea1a17ae
jebersownanlz-sys <jebersownanlz@gmail.com>
2026-06-09 19:26:56 -0500

    Add program to find minimum of two numbers

---
+++ ejercicio8.cs
@@ -0,0 +1,13 @@
+//Este método se puede utilizar para encontrar el valor mínimo de x e y :Math.Min(x,y)
+using System;
+
+namespace ejercicio8
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Min(5, 10));  
+     }
+  }
+}
